c6a84b7d1788540489afd413e0a3033f2adc5f34
sopobox0420 <gyu020420@gmail.com>
2026-04-26 12:35:18 +0900

    Update main.py

--- backend-prototype/main.py
+++ backend-prototype/main.py
@@ -20,7 +20,7 @@ def root():
     return {"message": "서버 정상 실행 중"}
 
 # 라우터 등록
-app.include_router(drug.router, prefix="/api")
+app.include_router(drug.router, prefix="/api", tags=["Drug"])
 app.include_router(chat.router, prefix="/api")
-app.include_router(alarm.router, prefix="/api")
+app.include_router(alarm.router, prefix="/api", tags=["Alarm"])
 app.include_router(map.router, prefix="/api/map", tags=["Map"])
